990c35e5afcbfdbc81a0cee761e5484972df1dea
flori <qwertz2002fr@yahoo.fr>
2022-06-03 11:34:45 +0200

    event binding

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -11,6 +11,8 @@ export class FaceSnapComponent implements OnInit {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapDone!: boolean;
+  buttonText!: string;
 
   ngOnInit(): void {
     this.title = 'Archibald';
@@ -19,10 +21,17 @@ export class FaceSnapComponent implements OnInit {
     this.snaps = 6;
 
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.buttonText = 'Oh Snap!';
   }
 
-  onAddSnap() {
-    this.snaps++;
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
   }
 
 }
