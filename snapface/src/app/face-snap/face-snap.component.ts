fe3c6dd9ecf1477bb71a5b69fb93e7c6a26c794b
flori <qwertz2002fr@yahoo.fr>
2022-06-15 16:57:06 +0200

    spa et routing

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
 
@@ -11,7 +12,8 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
 
   buttonText!: string;
-  constructor(private faceSnapsService: FaceSnapsService) { }
+  constructor(private router: Router, private faceSnapsService: FaceSnapsService) { }
+
 
   ngOnInit(): void {
     this.buttonText = 'Oh Snap!';
@@ -25,6 +27,10 @@ export class FaceSnapComponent implements OnInit {
       this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
       this.buttonText = 'Oh Snap!';
     }
+  }
+
+  onViewFaceSnap() {
+    this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
   }
 
 }
