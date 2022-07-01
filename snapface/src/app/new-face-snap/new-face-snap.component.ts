6826cce80c94c6bf9f68eb7f4bfe3b44df8d1906
flori <qwertz2002fr@yahoo.fr>
2022-07-01 12:18:52 +0200

    Formulaires réactifs

--- snapface/src/app/new-face-snap/new-face-snap.component.ts
+++ snapface/src/app/new-face-snap/new-face-snap.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -8,7 +10,10 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class NewFaceSnapComponent implements OnInit {
 
+  //...
   snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
+  //...
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -19,6 +24,15 @@ export class NewFaceSnapComponent implements OnInit {
       imageUrl: [null],
       location: [null]
     });
+
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+        ...formValue,
+        createdDate: new Date(),
+        snaps: 0,
+        id: 0
+      }))
+    );
   }
 
   onSubmitForm() {
